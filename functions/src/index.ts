06efbb4255098dab8a99d86a4105730492207fc7
Aibek <ai.bek@mail.ru>
2022-12-16 14:43:27 +0600

    feat: create create and get all ads api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,41 @@
 import * as functions from "firebase-functions";
+import * as express from "express";
+//import * as cors from "cors";
+import * as admin from "firebase-admin";
+import { IAdCreate, IAd, IGetAd} from "./models/rest-api-models";
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const app = express();
+
+//create ad
+app.post("/", async (req, res) => {
+  try {
+    const ad: IAdCreate = req.body;
+    await admin.firestore().collection("habar-ads").add(ad);
+    res.status(201).send('ad was created');
+  } catch (error) {
+    res.status(500).send()
+  }
+});
+
+//get all ads 
+app.get('/', async(req, res) => {
+  try {
+    const snapshot = await admin.firestore().collection('habar-ads').get()
+    let ads: IGetAd[] = []
+    
+    snapshot.forEach((doc) => {
+      const ad:IGetAd = {
+        id: doc.id,
+        ...doc.data() as IAd
+      }
+      ads.push(ad);
+    })
+    res.status(200).send(ads);
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+export const ads = functions.https.onRequest(app);
